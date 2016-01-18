e51efa2931684b3e49451da86a1211b73ac41abc
Marek <marekm2211@gmail.com>
2016-01-18 15:06:19 +0100

    REST API added

--- module/Mail/config/module.config.php
+++ module/Mail/config/module.config.php
@@ -8,25 +8,58 @@ return array(
 
     'router' => array(
         'routes' => array(
-            'home' => array(
-                'type'    => 'segment',
+            'mail' => array(
+                'type' => 'segment',
                 'options' => array(
-                    'route'    => '/[:id]',
+                    'route' => '/mail[/:id]',
                     'constraints' => array(
-                        'id'     => '[0-9]+',
+                        'id' => '[0-9]+',
                     ),
                     'defaults' => array(
                         'controller' => 'Mail\Controller\Mail',
-                        'action'=>'index',
                     ),
                 ),
+                'may_terminate' => true,
+                'child_routes' => [
+                    'dir' => [
+                        'type' => 'literal',
+                        'options' => [
+                            'route' => '/dir',
+                            'defaults' => [
+                                'action' => 'dir'
+                            ]
+                        ]
+                    ],
+                    'subdir' => [
+                        'type' => 'segment',
+                        'options' => [
+                            'route' => '/subdir/:folder',
+                            'defaults' => [
+                                'action' => 'subdir']
+                        ]
+                    ]
+                ]
             ),
         ),
     ),
 
     'view_manager' => array(
+        'display_not_found_reason' => true,
+        'display_exceptions' => true,
+        'doctype' => 'HTML5',
+        'not_found_template' => 'error/404',
+        'exception_template' => 'error/index',
+        'template_map' => array(
+//            'layout/layout' => __DIR__ . '/../view/layout/layout.phtml',
+//            'index/index' => __DIR__ . '/../view/index/index.phtml',
+//            'error/404' => __DIR__ . '/../view/error/404.phtml',
+//            'error/index' => __DIR__ . '/../view/error/index.phtml',
+        ),
         'template_path_stack' => array(
-            'mail' => __DIR__ . '/../view',
+            'application' => __DIR__ . '/../view',
+        ),
+        'strategies' => array(
+            'ViewJsonStrategy',
         ),
     ),
 );
--- module/Mail/src/Mail/Controller/MailController.php
+++ module/Mail/src/Mail/Controller/MailController.php
@@ -3,37 +3,86 @@
 
 namespace Mail\Controller;
 
-use Zend\Http\Header\TransferEncoding;
-use Zend\Mvc\Controller\AbstractActionController;
+use Zend\Json\Server\Response;
 use Zend\Mail\Storage\Imap as Imap;
 use Zend\Mail\Storage\Message;
+use Zend\Mvc\Controller\AbstractRestfulController;
+use Zend\View\Model\JsonModel;
 
-class MailController extends AbstractActionController
+class MailController extends AbstractRestfulController
 {
-    public function indexAction()
+
+    public function ConnectToMailBox()
     {
-        $method = $_SERVER['REQUEST_METHOD'];
-        $id = $this->params()->fromRoute('id', 0);
-        if (!$id) {
-            $serverAnswer = new Imap ([
-                'host' => 'imap.poczta.onet.pl',
-                'user' => 'marek.mikusek@onet.eu',
-                'password' => '123QWEasd',
-            ]);
-            $mails = [];
-            $i = 1;
-            foreach ($serverAnswer as $inputMail) {
-                $mail = $serverAnswer->getMessage($i);
-                $mails[$i]['date'] = $mail->date;
-                $mails[$i]['from'] = $mail->from;
-                $mails[$i]['subject'] = $mail->subject;
-                foreach ($mail as $part)
-                    $content = quoted_printable_decode($part->getContent());
-                $mails[$i]['content'] = $content;
-                $i++;
-            }
-            return ['mails' => json_encode($mails)];
+        return $serverAnswer = new Imap ([
+            'host' => 'imap.poczta.onet.pl',
+            'user' => 'marek.mikusek@onet.eu',
+            'password' => '123QWEasd',
+        ]);
+    }
+
+    private function showMails($folder)
+    {
+        $connectionToMailServer = $this->ConnectToMailBox();
+        $connectionToMailServer->selectFolder($folder);
+        $mails = [];
+
+        for ($i = 1; $i <= $connectionToMailServer->countMessages(); $i++) {
+            $mail = $connectionToMailServer->getMessage($i);
+            $mails[$i]['id'] = $i;
+            $mails[$i]['date'] = $mail->date;
+            $mails[$i]['from'] = $mail->from;
+            $mails[$i]['subject'] = $mail->subject;
+            foreach ($mail as $part)
+                $content = quoted_printable_decode($part->getContent());
+            $mails[$i]['content'] = $content;
         }
+
+        return $mails;
+    }
+
+    public function getList()
+    {
+        return new JsonModel($this->showMails("INBOX"));
+    }
+
+    public function get()
+    {
+        $id = (int)$this->params()->fromRoute('id', 0);
+        $connectionToMailBox = $this->ConnectToMailBox();
+        $mail = $connectionToMailBox->getMessage($id);
+
+        $mailToEdit['id'] = $id;
+        $mailToEdit['date'] = $mail->date;
+        $mailToEdit['from'] = $mail->from;
+        $mailToEdit['subject'] = $mail->subject;
+        foreach ($mail as $part)
+            $content = quoted_printable_decode($part->getContent());
+        $mailToEdit['content'] = $content;
+
+        return new JsonModel($mailToEdit);
+    }
+
+    public function delete($id)
+    {
+        $this->ConnectToMailBox()->removeMessage($id);
+        $this->getResponse()->setStatusCode(204);
+
+        return;
+    }
+
+    public function dirAction()
+    {
+        $folders = $this->ConnectToMailBox()->getFolders();
+        $return = [];
+        foreach ($folders as $folder)
+            $return[] = $folder->getGlobalName();
+        return new JsonModel($return);
+    }
+
+    public function subdirAction()
+    {
+        return new JsonModel($this->showMails($this->params()->fromRoute('folder')));
     }
 
 }
