196d564fba940fb3085d547ab0ee45ab8489cd0e
Marek <marekm2211@gmail.com>
2016-01-26 13:10:22 +0100

    addind attachments to sended emails added

--- public/prepareMailToSend.php
+++ public/prepareMailToSend.php
@@ -9,59 +9,26 @@ if ($_SERVER['REQUEST_METHOD'] == "GET") {
         <!-- Bootstrap -->
         <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
-        <!--         <script type="text/javascript">-->
-        <!--            $(document).ready(function () {-->
-        <!--                $("#submit_btn").click(function () {-->
-        <!---->
-        <!--                    var proceed = true;-->
-        <!--                    //simple validation at client's end-->
-        <!--                    //loop through each field and we simply change border color to red for invalid fields-->
-        <!--                    $("#contact_form input[required=true], #contact_form textarea[required=true]").each(function () {-->
-        <!--                        $(this).css('border-color', '');-->
-        <!--                        if (!$.trim($(this).val())) { //if this field is empty-->
-        <!--                            $(this).css('border-color', 'red'); //change border color to red-->
-        <!--                            proceed = false; //set do not proceed flag-->
-        <!--                        }-->
-        <!--                        //check invalid email-->
-        <!--                        var email_reg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;-->
-        <!--                        if ($(this).attr("type") == "email" && !email_reg.test($.trim($(this).val()))) {-->
-        <!--                            $(this).css('border-color', 'red'); //change border color to red-->
-        <!--                            proceed = false; //set do not proceed flag-->
-        <!--                        }-->
-        <!--                    });-->
-        <!---->
-        <!--                    if (proceed) //everything looks good! proceed...-->
-        <!--                    {-->
-        <!--                        //get input field values data to be sent to server-->
-        <!--                        post_data = {-->
-        <!--                            'user_name': $('input[name=name]').val(),-->
-        <!--                            'user_email': $('input[name=email]').val(),-->
-        <!--                            'subject': $('select[name=subject]').val(),-->
-        <!--                            'msg': $('textarea[name=message]').val()-->
-        <!--                        };-->
-        <!---->
-        <!--                        //Ajax post data to server-->
-        <!--                        $.post('contact_me.php', post_data, function (response) {-->
-        <!--                            if (response.type == 'error') { //load json data from server and output message-->
-        <!--                                output = '<div class="error">' + response.text + '</div>';-->
-        <!--                            } else {-->
-        <!--                                output = '<div class="success">' + response.text + '</div>';-->
-        <!--                                //reset values in all input fields-->
-        <!--                                $("#contact_form  input[required=true], #contact_form textarea[required=true]").val('');-->
-        <!--                                $("#contact_form #contact_body").slideUp(); //hide form after success-->
-        <!--                            }-->
-        <!--                            $("#contact_form #contact_results").hide().html(output).slideDown();-->
-        <!--                        }, 'json');-->
-        <!--                    }-->
-        <!--                });-->
-        <!---->
-        <!--                //reset previously set border colors and hide all message on .keyup()-->
-        <!--                $("#contact_form  input[required=true], #contact_form textarea[required=true]").keyup(function () {-->
-        <!--                    $(this).css('border-color', '');-->
-        <!--                    $("#result").slideUp();-->
-        <!--                });-->
-        <!--            });-->
-        <!--        </script>-->
+        <script type="text/javascript">
+            $(document).ready(function () {
+                $("#addAttachment").click(function (event) {
+                    event.preventDefault();
+                    $("#attachment").append(
+                        "<div class='form-group'>" +
+                        "<label for='attachment' class='col-sm-2 control-label'>Attachment</label>" +
+                            "<div class='col-sm-10'>" +
+                                "<input type='file' class='form-control' name='attachment[]' " +
+                            "</div> " +
+                        "</div>");
+                });
+
+                //reset previously set border colors and hide all message on .keyup()
+                $("#contact_form  input[required=true], #contact_form textarea[required=true]").keyup(function () {
+                    $(this).css('border-color', '');
+                    $("#result").slideUp();
+                });
+            });
+        </script>
     </head>
     <body>
     <div class="container">
@@ -99,13 +66,10 @@ if ($_SERVER['REQUEST_METHOD'] == "GET") {
                               placeholder="Message content"></textarea>
                 </div>
             </div>
+            <div id="attachment"></div>
             <div class="form-group">
-                <label for="attachment" class="col-sm-2 control-label">Attachment</label>
-                <div class="col-sm-10">
-                    <input type="file" class="form-control" id="attachment" name="attachment[]"
-                           placeholder="Attachment">
-                    <input type="file" class="form-control" id="attachment" name="attachment[]"
-                           placeholder="Attachment">
+                <div class="col-sm-offset-2 col-sm-10">
+                    <button type="submit" class="btn btn-default" id="addAttachment">Add attachment</button>
                 </div>
             </div>
             <div class="form-group">
